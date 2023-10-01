b3490c8f85d78e5e058c152b53dd524b0fdfa9a8
River <109698282+river-lockhart@users.noreply.github.com>
2023-09-30 23:58:00 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Lockhart Week 5'),
+      home: const MyHomePage(title: 'Week 6 Git'),
     );
   }
 }
